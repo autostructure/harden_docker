cb53f97b179824a041dd841db88f9d3e19ae57af
Bryan Belanger <bryan@autostructure.io>
2017-12-21 22:47:17 -0500

    ugh

--- manifests/config_daemon_keys.pp
+++ manifests/config_daemon_keys.pp
@@ -23,41 +23,41 @@ class harden_docker::config_daemon_keys {
   augeas { 'icc_key':
     changes => [
       'set dict/entry[last()+1] icc',
-      "set dict/entry[*][.='icc']/const ${allow_network_traffic_between_containers}",
+      "set dict/entry[.='icc']/const ${allow_network_traffic_between_containers}",
     ],
-    onlyif  => "match dict/entry[*][.='icc'] size == 0",
+    onlyif  => "match dict/entry[.='icc'] size == 0",
   }
 
   augeas { 'log-level_key':
     changes => [
       'set dict/entry[last()+1] log-level',
-      "set dict/entry[*][.='log-level']/string ${::harden_docker::set_the_logging_level}",
+      "set dict/entry[.='log-level']/string ${::harden_docker::set_the_logging_level}",
     ],
-    onlyif  => "match dict/entry[*][.='log-level'] size == 0",
+    onlyif  => "match dict/entry[.='log-level'] size == 0",
   }
 
   augeas { 'iptables_key':
     changes => [
       'set dict/entry[last()+1] iptables',
-      "set dict/entry[*][.='iptables']/const ${::harden_docker::allow_docker_to_make_changes_to_iptables}",
+      "set dict/entry[.='iptables']/const ${::harden_docker::allow_docker_to_make_changes_to_iptables}",
     ],
-    onlyif  => "match dict/entry[*][.='iptables'] size == 0",
+    onlyif  => "match dict/entry[.='iptables'] size == 0",
   }
 
   augeas { 'disable-legacy-registry_key':
     changes => [
       'set dict/entry[last()+1] disable-legacy-registry',
-      "set dict/entry[*][.='disable-legacy-registry']/const ${::harden_docker::disable_operations_on_legacy_registry}",
+      "set dict/entry[.='disable-legacy-registry']/const ${::harden_docker::disable_operations_on_legacy_registry}",
     ],
-    onlyif  => "match dict/entry[*][.='disable-legacy-registry'] size == 0",
+    onlyif  => "match dict/entry[.='disable-legacy-registry'] size == 0",
   }
 
   augeas { 'live_restore_key':
     changes => [
       'set dict/entry[last()+1] live-restore',
-      "set dict/entry[*][.='live-restore']/const ${::harden_docker::enable_live_restore}",
+      "set dict/entry[.='live-restore']/const ${::harden_docker::enable_live_restore}",
     ],
-    onlyif  => "match dict/entry[*][.='live-restore'] size == 0",
+    onlyif  => "match dict/entry[.='live-restore'] size == 0",
   }
 
   $enable_userland_proxys = $harden_docker::disable_userland_proxy ? {
@@ -68,8 +68,8 @@ class harden_docker::config_daemon_keys {
   augeas { 'userland-proxy_key':
     changes => [
       'set dict/entry[last()+1] userland-proxy',
-      "set dict/entry[*][.='userland-proxy']/const ${enable_userland_proxys}",
+      "set dict/entry[.='userland-proxy']/const ${enable_userland_proxys}",
     ],
-    onlyif  => "match dict/entry[*][.='userland-proxy'] size == 0",
+    onlyif  => "match dict/entry[.='userland-proxy'] size == 0",
   }
 }
